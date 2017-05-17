ddc3b193e62450e16cbdc7da922a6e79048540dd
DaveMaran <davidmaran@Davids-MacBook-Pro.local>
2017-05-17 14:49:19 +0100

    Services page css

--- page-templates/page-services.php
+++ page-templates/page-services.php
@@ -4,5 +4,87 @@ Template Name: Services
 */
 get_header(); ?>
 
+<div class="services__header">
+  <div class="services__header--content">
+    <h1>More than a<br />manufacturer</h1>
+    <h2>Design, stand hire, installation, storage and more</h2>
+    <p>Services introduction text needs writing. This is placeholder: The ISOframe Wave is a portable exhibition system with a display that can be formed into any shape and reduced or extended in size without the need to reprint graphics. The Wave is ideal for shell-schemes or space only events and where you need the flexibility to adapt to different size exhibition spaces.</p>
+  </div>
+</div>
+
+<div class="services__row--normal">
+  <div class="services__row--normal__image">
+   <img src="<?php echo get_template_directory_uri(); ?>/assets/images/service1.jpg ">
+  </div>
+
+  <div class="services__row--normal__text">
+    <h2>3D design</h2>
+    <p>This is placeholder text: We work closely with you to understand your exact requirements and realise your vision. We take care of everything but involve you at key stages to ensure you’re happy that everything is in line with your expectations and plans.</p>
+    <a href="#" class="button">Find out more</a>
+  </div>
+</div>
+
+<div class="services__row--coloured">
+  <div class="services__row--coloured--spacer">
+    <div class="services__row--coloured__image">
+     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/service2.jpg ">
+    </div>
+    
+    <div class="services__row--coloured__text">
+      <h2>3D design</h2>
+      <p>This is placeholder text: We work closely with you to understand your exact requirements and realise your vision. We take care of everything but involve you at key stages to ensure you’re happy that everything is in line with your expectations and plans.</p>
+      <a href="#" class="button">Find out more</a>
+    </div>
+  </div>
+</div>
+
+<div class="services__row--normal">
+  <div class="services__row--normal__image">
+   <img src="<?php echo get_template_directory_uri(); ?>/assets/images/service3.jpg ">
+  </div>
+
+  <div class="services__row--normal__text">
+    <h2>3D design</h2>
+    <p>This is placeholder text: We work closely with you to understand your exact requirements and realise your vision. We take care of everything but involve you at key stages to ensure you’re happy that everything is in line with your expectations and plans.</p>
+    <a href="#" class="button">Find out more</a>
+  </div>
+</div>
+
+<div class="services__row--coloured">
+  <div class="services__row--coloured--spacer">
+    <div class="services__row--coloured__image">
+     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/service4.jpg ">
+    </div>
+    
+    <div class="services__row--coloured__text">
+      <h2>3D design</h2>
+      <p>This is placeholder text: We work closely with you to understand your exact requirements and realise your vision. We take care of everything but involve you at key stages to ensure you’re happy that everything is in line with your expectations and plans.</p>
+      <a href="#" class="button">Find out more</a>
+    </div>
+  </div>
+</div>
+
+<div class="services__row--normal">
+  <div class="services__row--normal__image">
+   <img src="<?php echo get_template_directory_uri(); ?>/assets/images/service5.jpg ">
+  </div>
+
+  <div class="services__row--normal__text">
+    <h2>3D design</h2>
+    <p>This is placeholder text: We work closely with you to understand your exact requirements and realise your vision. We take care of everything but involve you at key stages to ensure you’re happy that everything is in line with your expectations and plans.</p>
+    <a href="#" class="button">Find out more</a>
+  </div>
+</div>
+
+<div class="footerCTAbg">
+  <div class="footerCTA">
+    <div class="footerCTA--content">
+    Have an upcoming exhibition but need help to maximise impact and make the most of your investment?
+    </div>
+    <div class="footerCTA--link">
+      <a href="#" class="button">We can help</a>
+    </div>
+  </div>
+</div>
 
 <?php get_footer();
