58efbcf51d30e86c5a41a937fa64f0d1160bda0b
DaveMaran <davidmaran@Davids-MacBook-Pro.local>
2017-05-17 14:10:28 +0100

    Contact page map styles

--- page-templates/page-contact.php
+++ page-templates/page-contact.php
@@ -5,7 +5,7 @@ Template Name: contact page
 get_header(); ?>
 
 <div class="map">
-  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2411.1313252416408!2d-1.769082783694628!3d52.81999672170489!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487a0509b57dbe5b%3A0xb3191e90a0a4a40a!2sISOframe%2C+by+Display+Makers!5e0!3m2!1sen!2suk!4v1495025882125" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2411.1313252416408!2d-1.769082783694628!3d52.81999672170489!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487a0509b57dbe5b%3A0xb3191e90a0a4a40a!2sISOframe%2C+by+Display+Makers!5e0!3m2!1sen!2suk!4v1495025882125" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
 
 <div class="contact--page">
