12da5b8b3d2a54158bc62130c4da158f737212d7
DaveMaran <davidmaran@Davids-MacBook-Pro.local>
2017-05-18 15:07:01 +0100

    Base page HTML

--- taxonomy-product_type.php
+++ taxonomy-product_type.php
@@ -11,34 +11,76 @@
  */
 
  get_header(); ?>
-testtest
-<div class="single--featured">
-	<?php
-		if ( has_post_thumbnail() ) {
-			the_post_thumbnail();
-		} 
-	?>
-</div>
- <div class="page--default">
 
-
- <?php while ( have_posts() ) : the_post(); ?>
-  <div class="content">
-  	<h1><?php the_title(); ?></h1>
-           <?php the_content(); ?>
-	</div>
- <?php endwhile;?>
-<div class="sidebar">
-	<h1>Category</h1>
+<div class="productMenu">
 	<ul>
-    <?php wp_list_categories( array(
-        'orderby'    => 'name',
-        'show_count' => false,
-        'title_li' => false,
-    ) ); ?> 
-</ul>
+		<li><a href="#">Portable</a></li>
+		<li><a href="#">Modular</a></li>
+		<li><a href="#">Custom</a></li>
+		<li><a href="#">Counters and accessories</a></li>
+	</ul>
 </div>
 
- </div>
+<div class="productType--featuredImage">
+	<img src="#">
+</div>
 
+<div class="productType--heading">
+	<h1>Modular exhibition stands</h1>
+</div>
+
+<div class="productType--features">
+	<ul>
+		<li class="ultra-light">Ultra-light</li>	
+		<li class="tool-less">Tool-less assembly</li>
+		<li class="flexible">Flexible</li>
+		<li class="quick-asemble">Quick to assemble</li>
+		<li class="cost-effective">Cost-effective</li>
+	</ul>
+</div>
+
+<div class="productType__content">
+<h2>What are ISOframe Modular Solutions?</h2>
+	<div class="productType__content--main">
+		<p>Flexible exhibition stand systems that can easily be re-configured, transported and re-used for multiple events.</p>
+
+		<p>This text should be features and benefits of all modular systems. Needs writing so the following is placeholder text. Our Installed Solutions packages include an ISOframe exhibition stand, installation at any venue in mainland UK and graphics designed by our in-house creative team.</p>
+
+		<p>All you have to do is choose a package based on what you need; a range of different size stands are available as well as options on accessories including media screens and furniture. We can also arrange breakdown and removal from your venue if required.</p>
+	</div>
+
+	<div class="productType__content--pullout">
+		<h3>Customer service promise</h3>
+
+		<p>Placeholder text: With over 50 years experience of delivering stands of every type for our clients, we understand all the ingredients that are essential for exhibition success so you can rely on us to ensure that everything is in place. </p>
+
+		<p>The difference is in the detail. We understand the little things that can make a big difference and which can deliver the Wow! factor you are looking for.</p>
+	</div>
+</div>
+
+<div class="productType__action">
+	<div class="productType__action--box">
+		<img src="#">
+		Exhibit
+		<a href="#">Find out more ></a>
+	</div>
+	<div class="productType__action--box">
+		<img src="#">
+		Wave
+		<a href="#">Find out more ></a>
+	</div>
+	<div class="productType__action--box">
+		<img src="#">
+		Fabric
+		<a href="#">Find out more ></a>
+	</div>
+	<div class="productType__action--box">
+		<h3>Not sure which of our Modular products is the best option for you?</h3>
+		<p>Just call <a href="tel:01283 575097">01283 575097</a> or email <a href="mailto:sales@isoframe.co.uk">sales@isoframe.co.uk</a> for help and advice</p>
+	</div>
+</div>
+<div class="productType-CTA">
+	<h2>Have an upcoming exhibition but need help to maximise impact and make the most of your investment?</h2>
+	<a href="#" class="button">Inspire me</a>
+</div>
  <?php get_footer();
