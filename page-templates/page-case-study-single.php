14fd320df4760a22a88684f28eb7e1686d4651a7
DaveMaran <davidmaran@Davids-MacBook-Pro.local>
2017-05-26 19:10:02 +0100

    Case study single

---
+++ page-templates/page-case-study-single.php
@@ -0,0 +1,92 @@
+<?php
+/*
+Template Name: Case study Single
+*/
+get_header(); ?>
+
+
+<?php include('template-parts/header-cta.php'); ?>
+
+<div class="caseStudy-single__Image">
+  <img src="#">
+
+  <div class="aseStudy-single__Image--option" style="position:absolute; top:0px; left:0px;">
+    <p>Text Here</p>
+  </div>
+
+  <div class="caseStudy-single__Image--option" style="position:absolute; top:0px; left:0px;">
+    <p>Text Here</p>
+  </div>
+
+  <div class="caseStudy-single__Image--option" style="position:absolute; top:0px; left:0px;">
+    <p>Text Here</p>
+  </div>
+
+  <div class="caseStudy-single__Image--option" style="position:absolute; top:0px; left:0px;">
+    <p>Text Here</p>
+  </div>
+</div>
+
+<div class="caseStudy-single__featuredSection">
+
+<h2>Why Best Western reduced their exhibition costs by 50% using ISOframe custom</h2>
+
+  <div class="caseStudy-single__featuredSection--content">
+    <img src="#">
+
+    <h2>About best western</h2>
+    <p>Best Western is one of the largest independent hotel chains in the UK, forming part of a group that owns and manages 4,000 hotels in over 80 countries worldwide.</p>
+  </div>
+
+  <div class="caseStudy-single__featuredSection--sidebar">
+    <img src="#">
+
+    <div>
+      <h3>Sector</h3>
+      <span>Leisure</span>
+    </div>
+
+    <div>
+      <h3>Shows</h3>
+      <ol>
+        <li>Show 1</li>
+        <li>Show 2</li>
+        <li>Show 3</li>
+        <li>Show 4</li>
+      </ol>
+    </div>
+
+  </div>
+</div>
+
+<div class="caseStudy-single__section--first">
+  <div class="caseStudy-single__section--first__content">
+    <h2>The Challenge</h2>
+    <p>Best Western had been exhibiting for years. They understood that making an impact involved significant investment but it didn’t seem right to have to throw away their stand each time they exhibited, especially after they had spent valuable time and money getting their design just right.</p>
+    
+    <p>They had 3 distinctive brands, aimed at different markets and a minimum of 6 exhibitions a year. The design needed to be distinctive and welcoming but practical at the same time. </p>
+    <p>The team wanted sophisticated curves, high quality bespoke accessories and individuality.</p>
+  </div>
+  <div class="caseStudy-single__section--first__image">
+    <img src="#">
+  </div>
+</div>
+
+<div class="caseStudy-single__CTA">
+  <h2>Title here</h2>
+  <a href="#"" class="button">Go here</a>
+</div>
+
+<div class="caseStudy-single__testimonial">
+  <div>
+      <img src="#">
+      <p>“We had thought using a shell scheme made life simple, until we discovered ISOframe’s Custom system. Changing to ISOframe Custom means our exhibition programme is now pain-free and importantly, delivering great return on our investment.”</p>
+      <p>Author Here/p>
+  </div>
+</div>
+
+<?php get_template_part( 'template-parts/news-advice', 'news-advice' ); ?>
+
+<?php get_template_part( 'template-parts/footer-cta', 'footer-cta' ); ?>
+
+<?php get_footer();
