282d530e2aadd2562ffcb8d7158989d0eebc3e1a
DaveMaran <davidmaran@Davids-MacBook-Pro.local>
2017-05-26 19:17:56 +0100

    blog page

---
+++ page-templates/page-blog.php
@@ -0,0 +1,106 @@
+<?php
+/*
+Template Name: Blog
+*/
+get_header(); ?>
+
+<div class="blog__featured">
+
+  <div class="blog__featured--image">
+    <img src="#">
+  </div>
+  <div class="blog__featured--content">
+    <h2>Title here</h2>
+    <p>Create ground-breaking exhibitions by following our tips and staying on-trend in 2017</p>
+    <a href="#" class="button--red">Read More</a>
+  </div>
+</div>
+
+<div class="blog__grid">
+<h2>
+  <div class="blog__grid--content">
+
+    <div>Blog Articles</div>
+      <img src='#'>
+      <h2>Title</h2>
+      <a href='# '>Read more ></a>
+    </div>
+
+    <div>
+      <img src='#'>
+      <h2>Title</h2>
+      <a href='# '>Read more ></a>
+    </div>
+
+    <div>
+      <img src='#'>
+      <h2>Title</h2>
+      <a href='# '>Read more ></a>
+    </div>
+
+    <div>
+      <img src='#'>
+      <h2>Title</h2>
+      <a href='# '>Read more ></a>
+    </div>
+
+    <div>
+      <img src='#'>
+      <h2>Title</h2>
+      <a href='# '>Read more ></a>
+    </div>
+
+    <div>
+      <img src='#'>
+      <h2>Title</h2>
+      <a href='# '>Read more ></a>
+    </div>
+
+  </div>
+</div>
+
+<div class="blog__news-grid">
+<h2>
+  <div class="blog__news-grid--content">
+
+    <div>Latest News</div>
+      <img src='#'>
+      <h2>Title</h2>
+      <a href='# '>Read more ></a>
+    </div>
+
+    <div>
+      <img src='#'>
+      <h2>Title</h2>
+      <a href='# '>Read more ></a>
+    </div>
+
+    <div>
+      <img src='#'>
+      <h2>Title</h2>
+      <a href='# '>Read more ></a>
+    </div>
+
+    <div>
+      <img src='#'>
+      <h2>Title</h2>
+      <a href='# '>Read more ></a>
+    </div>
+
+    <div>
+      <img src='#'>
+      <h2>Title</h2>
+      <a href='# '>Read more ></a>
+    </div>
+
+    <div>
+      <img src='#'>
+      <h2>Title</h2>
+      <a href='# '>Read more ></a>
+    </div>
+
+  </div>
+</div>
+<?php get_template_part( 'template-parts/footer-cta', 'footer-cta' ); ?>
+
+<?php get_footer();
