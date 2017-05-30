88dd0f60a7a18b2b4a1ca9e6feffaa3245dc7964
DaveMaran <davidmaran@davids-mbp.home>
2017-05-30 11:50:10 +0100

    Blog page coded

--- page-templates/page-blog.php
+++ page-templates/page-blog.php
@@ -5,53 +5,53 @@ Template Name: Blog
 get_header(); ?>
 
 <div class="blog__featured">
-
-  <div class="blog__featured--image">
-    <img src="#">
-  </div>
-  <div class="blog__featured--content">
-    <h2>Title here</h2>
-    <p>Create ground-breaking exhibitions by following our tips and staying on-trend in 2017</p>
-    <a href="#" class="button--red">Read More</a>
+  <div>
+    <div class="blog__featured--image">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/blog-image.jpg">
+    </div>
+    <div class="blog__featured--content">
+      <h2>Title here</h2>
+      <p>Create ground-breaking exhibitions by following our tips and staying on-trend in 2017</p>
+      <a href="#" class="button--red">Read More</a>
+    </div>
   </div>
 </div>
 
 <div class="blog__grid">
-<h2>
+<h2>Blog Articles</h2>
   <div class="blog__grid--content">
-
-    <div>Blog Articles</div>
-      <img src='#'>
+  <div>
+      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
       <h2>Title</h2>
       <a href='# '>Read more ></a>
     </div>
 
     <div>
-      <img src='#'>
+      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
       <h2>Title</h2>
       <a href='# '>Read more ></a>
     </div>
 
     <div>
-      <img src='#'>
+      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
       <h2>Title</h2>
       <a href='# '>Read more ></a>
     </div>
 
     <div>
-      <img src='#'>
+      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
       <h2>Title</h2>
       <a href='# '>Read more ></a>
     </div>
 
     <div>
-      <img src='#'>
+      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
       <h2>Title</h2>
       <a href='# '>Read more ></a>
     </div>
 
     <div>
-      <img src='#'>
+      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
       <h2>Title</h2>
       <a href='# '>Read more ></a>
     </div>
@@ -60,45 +60,46 @@ get_header(); ?>
 </div>
 
 <div class="blog__news-grid">
-<h2>
+<h2>Latest News</h2>
   <div class="blog__news-grid--content">
 
-    <div>Latest News</div>
-      <img src='#'>
+  <div>
+      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
       <h2>Title</h2>
       <a href='# '>Read more ></a>
     </div>
 
     <div>
-      <img src='#'>
+      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
       <h2>Title</h2>
       <a href='# '>Read more ></a>
     </div>
 
     <div>
-      <img src='#'>
+      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
       <h2>Title</h2>
       <a href='# '>Read more ></a>
     </div>
 
     <div>
-      <img src='#'>
+      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
       <h2>Title</h2>
       <a href='# '>Read more ></a>
     </div>
 
     <div>
-      <img src='#'>
+      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
       <h2>Title</h2>
       <a href='# '>Read more ></a>
     </div>
 
     <div>
-      <img src='#'>
+      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
       <h2>Title</h2>
       <a href='# '>Read more ></a>
     </div>
 
+  </div>
   </div>
 </div>
 <?php get_template_part( 'template-parts/footer-cta', 'footer-cta' ); ?>
