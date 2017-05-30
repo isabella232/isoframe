0082383c1280ab02daa9ed39c9b7cf509e50fe85
DaveMaran <davidmaran@davids-mbp.home>
2017-05-30 11:27:12 +0100

    Case study single page

--- page-templates/page-case-study-single.php
+++ page-templates/page-case-study-single.php
@@ -8,22 +8,22 @@ get_header(); ?>
 <?php include('template-parts/header-cta.php'); ?>
 
 <div class="caseStudy-single__Image">
-  <img src="#">
+  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/inspiriation-custom.png">
 
-  <div class="aseStudy-single__Image--option" style="position:absolute; top:0px; left:0px;">
-    <p>Text Here</p>
+  <div class="caseStudy-single__Image--option" style="margin-left:-50px; top:520px;">
+    <div class="arrow_box">Text Here</div>
   </div>
 
-  <div class="caseStudy-single__Image--option" style="position:absolute; top:0px; left:0px;">
-    <p>Text Here</p>
+  <div class="caseStudy-single__Image--option" style="margin-left:-420px; top:300px;">
+    <div class="arrow_box">Text Here</div>
   </div>
 
-  <div class="caseStudy-single__Image--option" style="position:absolute; top:0px; left:0px;">
-    <p>Text Here</p>
+  <div class="caseStudy-single__Image--option" style="margin-left:350px; top:100px;">
+    <div class="arrow_box">Text Here</div>
   </div>
 
-  <div class="caseStudy-single__Image--option" style="position:absolute; top:0px; left:0px;">
-    <p>Text Here</p>
+  <div class="caseStudy-single__Image--option" style="margin-left:-60px; top:20px;">
+    <div class="arrow_box">Text Here</div>
   </div>
 </div>
 
@@ -32,14 +32,14 @@ get_header(); ?>
 <h2>Why Best Western reduced their exhibition costs by 50% using ISOframe custom</h2>
 
   <div class="caseStudy-single__featuredSection--content">
-    <img src="#">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/best-western.jpg">
 
     <h2>About best western</h2>
     <p>Best Western is one of the largest independent hotel chains in the UK, forming part of a group that owns and manages 4,000 hotels in over 80 countries worldwide.</p>
   </div>
 
   <div class="caseStudy-single__featuredSection--sidebar">
-    <img src="#">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/bestwesternlogo.png">
 
     <div>
       <h3>Sector</h3>
@@ -60,28 +60,48 @@ get_header(); ?>
 </div>
 
 <div class="caseStudy-single__section--first">
-  <div class="caseStudy-single__section--first__content">
-    <h2>The Challenge</h2>
-    <p>Best Western had been exhibiting for years. They understood that making an impact involved significant investment but it didn’t seem right to have to throw away their stand each time they exhibited, especially after they had spent valuable time and money getting their design just right.</p>
-    
-    <p>They had 3 distinctive brands, aimed at different markets and a minimum of 6 exhibitions a year. The design needed to be distinctive and welcoming but practical at the same time. </p>
-    <p>The team wanted sophisticated curves, high quality bespoke accessories and individuality.</p>
+  <div>
+    <div class="caseStudy-single__section--first__content">
+      <h2>The Challenge</h2>
+      <p>Best Western had been exhibiting for years. They understood that making an impact involved significant investment but it didn’t seem right to have to throw away their stand each time they exhibited, especially after they had spent valuable time and money getting their design just right.</p>
+      
+      <p>They had 3 distinctive brands, aimed at different markets and a minimum of 6 exhibitions a year. The design needed to be distinctive and welcoming but practical at the same time. </p>
+      <p>The team wanted sophisticated curves, high quality bespoke accessories and individuality.</p>
+    </div>
+    <div class="caseStudy-single__section--first__image">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/bestwestern1.jpg">
+    </div>
   </div>
-  <div class="caseStudy-single__section--first__image">
-    <img src="#">
+</div>
+
+<div class="caseStudy-single__section--second">
+  <div>
+
+    <div class="caseStudy-single__section--second__image">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/bestwestern2.jpg">
+    </div>
+
+    <div class="caseStudy-single__section--second__content">
+      <h2>The Isoframe Solution</h2>
+      <p>Best Western’s requirements for multiple configurations were complex. Fortunately, the patented ISOframe Custom system, is strong, immensely durable and innately flexible.</p>
+      <p>This meant that we designed 3 distinctive frameworks for each of the 3 brands using one ISOframe Custom system, modifying each structure to the exact dimensions stipulated by individual brand and venue requirements.</p>
+      <p>Our design team created a lifelike exhibition space, a real home-away-from-home. Visitors could chat with the sales team in comfort whilst charging their devices or connecting to WiFi to check urgent emails. </p>
+      <p>True to the brief, they incorporated practical features such as secure walk-in storage and integrated the stand’s electrics and multiple media screens without compromising the look and feel of an authentic Best Western lobby. </p><br />
+       <img src="<?php echo get_template_directory_uri(); ?>/assets/images/custom-logo.png">
+    </div>
   </div>
 </div>
 
 <div class="caseStudy-single__CTA">
-  <h2>Title here</h2>
-  <a href="#"" class="button">Go here</a>
+  <h2>They used a ISOframe Custom exhibition stand. This is intelligent exhibiting. Are you planning an exhibition?</h2>
+  <a href="#"" class="button">contact us</a>
 </div>
 
 <div class="caseStudy-single__testimonial">
   <div>
       <img src="#">
-      <p>“We had thought using a shell scheme made life simple, until we discovered ISOframe’s Custom system. Changing to ISOframe Custom means our exhibition programme is now pain-free and importantly, delivering great return on our investment.”</p>
-      <p>Author Here/p>
+      <p>“We had thought using a shell scheme made life simple, until we discovered ISOframe’s Custom system. Changing to ISOframe Custom means our exhibition programme is now pain-free and importantly, delivering great return on our investment.”
+      <span>Author Here</span></p>
   </div>
 </div>
 
