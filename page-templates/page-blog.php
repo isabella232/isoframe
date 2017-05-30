56c35ada3138c4b62fecb7da21eb97dc40856bb8
DaveMaran <davidmaran@davids-mbp.home>
2017-05-30 17:57:51 +0100

    Blog page

--- page-templates/page-blog.php
+++ page-templates/page-blog.php
@@ -5,99 +5,77 @@ Template Name: Blog
 get_header(); ?>
 
 <div class="blog__featured">
+<?php 
+    query_posts(array( 
+        'post_type' => 'posts',
+        'showposts' => 1,
+        'cat' => 9
+    ) );  
+?>
+<?php while (have_posts()) : the_post(); ?>
   <div>
     <div class="blog__featured--image">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/blog-image.jpg">
+     <?php 
+            if ( has_post_thumbnail() ) {
+              the_post_thumbnail();
+            }  
+          ?>
     </div>
     <div class="blog__featured--content">
-      <h2>Title here</h2>
-      <p>Create ground-breaking exhibitions by following our tips and staying on-trend in 2017</p>
-      <a href="#" class="button--red">Read More</a>
+      <h2><?php the_title(); ?></h2>
+      <p><?php echo get_the_excerpt(); ?></p>
+      <a href="<?php the_permalink(); ?>" class="button--red">Read More</a>
     </div>
   </div>
+  <?php endwhile;?>
 </div>
 
 <div class="blog__grid">
 <h2>Blog Articles</h2>
   <div class="blog__grid--content">
+<?php 
+    query_posts(array( 
+       'post_type' => 'posts',
+        'showposts' => 9,
+        'cat' => 9
+        'offset' => 1
+    ) );  
+?>
+<?php while (have_posts()) : the_post(); ?>
   <div>
-      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
-      <h2>Title</h2>
-      <a href='# '>Read more ></a>
+           <?php 
+            if ( has_post_thumbnail() ) {
+              the_post_thumbnail();
+            }  
+          ?>
+        <h2><?php the_title(); ?></h2>
+      <a href='<?php the_permalink(); ?>'>Read more ></a>
     </div>
-
-    <div>
-      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
-      <h2>Title</h2>
-      <a href='# '>Read more ></a>
-    </div>
-
-    <div>
-      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
-      <h2>Title</h2>
-      <a href='# '>Read more ></a>
-    </div>
-
-    <div>
-      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
-      <h2>Title</h2>
-      <a href='# '>Read more ></a>
-    </div>
-
-    <div>
-      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
-      <h2>Title</h2>
-      <a href='# '>Read more ></a>
-    </div>
-
-    <div>
-      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
-      <h2>Title</h2>
-      <a href='# '>Read more ></a>
-    </div>
-
+ <?php endwhile;?>
   </div>
 </div>
 
 <div class="blog__news-grid">
 <h2>Latest News</h2>
   <div class="blog__news-grid--content">
-
+<?php 
+    query_posts(array( 
+       'post_type' => 'posts',
+        'showposts' => 9,
+        'cat' => 10
+    ) );  
+?>
+<?php while (have_posts()) : the_post(); ?>
   <div>
-      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
-      <h2>Title</h2>
-      <a href='# '>Read more ></a>
+           <?php 
+            if ( has_post_thumbnail() ) {
+              the_post_thumbnail();
+            }  
+          ?>
+        <h2><?php the_title(); ?></h2>
+      <a href='<?php the_permalink(); ?>'>Read more ></a>
     </div>
-
-    <div>
-      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
-      <h2>Title</h2>
-      <a href='# '>Read more ></a>
-    </div>
-
-    <div>
-      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
-      <h2>Title</h2>
-      <a href='# '>Read more ></a>
-    </div>
-
-    <div>
-      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
-      <h2>Title</h2>
-      <a href='# '>Read more ></a>
-    </div>
-
-    <div>
-      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
-      <h2>Title</h2>
-      <a href='# '>Read more ></a>
-    </div>
-
-    <div>
-      <img src='<?php echo get_template_directory_uri(); ?>/assets/images/blog-sample.png'>
-      <h2>Title</h2>
-      <a href='# '>Read more ></a>
-    </div>
+ <?php endwhile;?>
 
   </div>
   </div>
