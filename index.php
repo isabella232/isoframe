dc62997a8fa1417b465b5e7aadac37515be7011e
DaveMaran <davidmaran@davids-mbp.home>
2017-05-31 12:27:57 +0100

    Index page

--- index.php
+++ index.php
@@ -1,27 +1,83 @@
 <?php
-/**
- * The template for displaying pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other "pages" on your WordPress site will use a different template.
- *
- * @package FoundationPress
- * @since FoundationPress 1.0.0
- */
 
- get_header(); ?>
+get_header(); ?>
 
- <div class="page--default">
+<div class="blog__featured">
+<?php 
+    query_posts(array( 
+        'post_type' => 'posts',
+        'showposts' => 1,
+        'cat' => 9
+    ) );  
+?>
+<?php while (have_posts()) : the_post(); ?>
+  <div>
+    <div class="blog__featured--image">
+     <?php 
+            if ( has_post_thumbnail() ) {
+              the_post_thumbnail();
+            }  
+          ?>
+    </div>
+    <div class="blog__featured--content">
+      <h2><?php the_title(); ?></h2>
+      <p><?php echo get_the_excerpt(); ?></p>
+      <a href="<?php the_permalink(); ?>" class="button--red">Read More</a>
+    </div>
+  </div>
+  <?php endwhile;?>
+</div>
 
- <?php while ( have_posts() ) : the_post(); ?>
-  <div class="content">
-  	<h1><?php the_title(); ?></h1>
-           <?php the_excerpt(); ?>
-	</div>
+<div class="blog__grid">
+<h2>Blog Articles</h2>
+  <div class="blog__grid--content">
+<?php 
+    query_posts(array( 
+       'post_type' => 'posts',
+        'showposts' => 9,
+        'cat' => 9
+        'offset' => 1
+    ) );  
+?>
+<?php while (have_posts()) : the_post(); ?>
+  <div>
+           <?php 
+            if ( has_post_thumbnail() ) {
+              the_post_thumbnail();
+            }  
+          ?>
+        <h2><?php the_title(); ?></h2>
+      <a href='<?php the_permalink(); ?>'>Read more ></a>
+    </div>
+ <?php endwhile;?>
+  </div>
+</div>
+
+<div class="blog__news-grid">
+<h2>Latest News</h2>
+  <div class="blog__news-grid--content">
+<?php 
+    query_posts(array( 
+       'post_type' => 'posts',
+        'showposts' => 9,
+        'cat' => 10
+    ) );  
+?>
+<?php while (have_posts()) : the_post(); ?>
+  <div>
+           <?php 
+            if ( has_post_thumbnail() ) {
+              the_post_thumbnail();
+            }  
+          ?>
+        <h2><?php the_title(); ?></h2>
+      <a href='<?php the_permalink(); ?>'>Read more ></a>
+    </div>
  <?php endwhile;?>
 
+  </div>
+  </div>
+</div>
+<?php get_template_part( 'template-parts/footer-cta', 'footer-cta' ); ?>
 
- </div>
-
- <?php get_footer();
+<?php get_footer();
