02fecaaab74db5d74e59a0fb667379e71e817003
DaveMaran <davidmaran@davids-mbp.home>
2017-05-31 15:49:24 +0100

    Term id images

--- archive-products.php
+++ archive-products.php
@@ -134,7 +134,7 @@ foreach ($terms as $term) {
   ?>
 <div class="frameFinder__results">
   <div class="frameFinder__results--image">
-  <?php the_field('category_image', 'term_' . $term->id); ?>
+  <img src="<?php the_field('category_image', 'term_' . $term->term_id); ?>">
   </div>
   <div class="frameFinder__results--content">
     <h2> <?php echo $term->name; ?></h2>
