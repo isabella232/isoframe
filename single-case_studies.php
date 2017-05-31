d79ac27b2eb5e9bb1d702efeebc786a7b377e48b
DaveMaran <davidmaran@Davids-MacBook-Pro.local>
2017-05-31 11:02:24 +0100

    Case studies page in Wordpress

---
+++ single-case_studies.php
@@ -0,0 +1,88 @@
+<?php
+/*
+Template Name: Case study Single
+*/
+get_header(); ?>
+
+
+<?php include('template-parts/header-cta.php'); ?>
+
+<div class="caseStudy-single__Image">
+  <img src="<?php the_field('image'); ?>">
+
+<?php if( have_rows('points') ):  while ( have_rows('points') ) : the_row(); ?>
+ <div class="caseStudy-single__Image--option" style="margin-left:<?php the_sub_field('left') ?>; top:<?php the_sub_field('top') ?>;">
+    <div class="arrow_box"><?php the_sub_field('content', false, false) ?></div>
+  </div>
+<?php endwhile; endif; ?>
+</div>
+
+<div class="caseStudy-single__featuredSection">
+
+<h2><?php the_field('openning_title'); ?></h2>
+
+  <div class="caseStudy-single__featuredSection--content">
+    <img src="<?php the_field('main_image'); ?>">
+
+    <?php the_field('content'); ?>
+  </div>
+
+  <div class="caseStudy-single__featuredSection--sidebar">
+    <img src="<?php the_field('client_logo'); ?>">
+
+    <div>
+      <h3>Sector</h3>
+      <span><?php the_field('sector'); ?></span>
+    </div>
+
+    <div>
+      <h3>Shows</h3>
+      <?php the_field('upcoming_shows'); ?>
+    </div>
+
+  </div>
+</div>
+
+<div class="caseStudy-single__section--first">
+  <div>
+    <div class="caseStudy-single__section--first__content">
+      <h2>The Challenge</h2>
+      <?php the_field('challenge'); ?>
+    </div>
+    <div class="caseStudy-single__section--first__image">
+      <img src="<?php the_field('challenge_image'); ?>">
+    </div>
+  </div>
+</div>
+
+<div class="caseStudy-single__section--second">
+  <div>
+
+    <div class="caseStudy-single__section--second__image">
+      <img src="<?php the_field('solution_image'); ?>">
+    </div>
+
+    <div class="caseStudy-single__section--second__content">
+      <?php the_field('main_image'); ?>
+    </div>
+  </div>
+</div>
+
+<div class="caseStudy-single__CTA">
+  <h2><?php the_field('cta_text'); ?></h2>
+  <a href="<?php echo site_url(); ?>/contact"" class="button">contact us</a>
+</div>
+
+<div class="caseStudy-single__testimonial">
+  <div>
+      <img src="<?php the_field('testimonial_image'); ?>">
+      <p><?php the_field('testimonial'); ?>
+      <span><?php the_field('testimonial_author'); ?></span></p>
+  </div>
+</div>
+
+<?php get_template_part( 'template-parts/news-advice', 'news-advice' ); ?>
+
+<?php get_template_part( 'template-parts/footer-cta', 'footer-cta' ); ?>
+
+<?php get_footer();
