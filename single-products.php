663d16df57c7ea8f1fb211a2433a196283367839
DaveMaran <davidmaran@Davids-MacBook-Pro.local>
2017-06-06 09:50:44 +0100

    Single product frame

--- single-products.php
+++ single-products.php
@@ -11,24 +11,32 @@
  */
 
  get_header(); ?>
-<div class="color--purple productsPage">
 
-	<div class="productHeader">
-		<div class="productHeader__content">
-			<div class="productHeader__content--text">
-			<img src="<?php the_field('logo'); ?>">
-				<h2><?php the_field('slogan_text'); ?></h2>
-			</div>
-			<div class="productHeader__content--image">
-			<img src="<?php the_field('image'); ?>">
+ <div class="slider">
+
+	<?php
+	if( have_rows('slider') ):
+	    while ( have_rows('slider') ) : the_row();
+	?>
+
+		<div style="background-image:url('<?php the_sub_field('background_image'); ?>');">
+		<img src="<?php the_sub_field('foreground_image'); ?>">
+			<div class="titleText">
+				<div>
+					<h2><?php the_sub_field('title_text', false); ?></h2>
+					<?php the_sub_field('sub_text', false); ?>
+				</div>
 			</div>
 		</div>
-		<div class="productHeader__mainText">
-			<div>
-				<p><?php the_field('openning_caption', false); ?></p>
-			</div>
-		</div>
-	</div>
+
+	<?php  
+		endwhile;
+	endif;
+	?>
+
+</div>
+
+<div class="color--purple productsPage">
 
 	<div class="product__content">
 		<div>
