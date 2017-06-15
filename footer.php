65eb73f4cdb7bb76613126135f92eecee4e69f89
DaveMaran <davidmaran@davids-mbp.home>
2017-06-15 10:58:32 +0100

    Fade...

--- footer.php
+++ footer.php
@@ -100,10 +100,10 @@ $('.searchIcon').click(function() {
 <script>
 
 $("#menu-item-148, .navigationProducts").hover(function() {  
-    $(this).stop().fadeTo('fast',1); 
+    $(this).stop().fadeTo('fast',0.5); 
     $(".navigationProducts").stop().slideDown(); 
 }, function() {  
-    $(this).stop().fadeTo('fast',0.1); 
+    $(this).stop().fadeTo('fast',1); 
     $(".navigationProducts").stop().slideUp(); 
 });
 
