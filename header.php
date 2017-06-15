5e41ff37343c829985e5ff5b624b203aa56092e0
DaveMaran <davidmaran@davids-mbp.home>
2017-06-15 10:39:03 +0100

    header drop down location

--- header.php
+++ header.php
@@ -51,37 +51,4 @@
 	</header>
 </div>
 
-<div class="navigationProducts">
-		<a href="#"> View all products </a>
-	<div>
-
-		<div class="productColumn">
-			<h2>Custom Exhibition Stands</h2>
-			<ul>
-				<li><a href="#">Custom</a></li>
-				<li><a href="#">Custom hire</a></li>
-			</ul>
-		</div>
-		<div class="productColumn">
-			<h2>Modular Exhibition Stands</h2>
-			<ul>
-				<li><a href="#">Wave</a></li>
-				<li><a href="#">Fabric</a></li>
-			</ul>
-		</div>
-		<div class="productColumn">
-			<h2>Portable Displays</h2>
-			<ul>
-				<li><a href="#">Ripple</a></li>
-				<li><a href="#">Express pop-up</a></li>
-			</ul>
-		</div>
-		<div class="productColumn">
-			<h2>Counters and accessories</h2>
-			<ul>
-				<li><a href="#">ISObar</a></li>
-			</ul>
-		</div>
-	</div>
-</div>
 <?php get_template_part( 'template-parts/header-cta', 'headercta' ); ?>
--- template-parts/mobile-top-bar.php
+++ template-parts/mobile-top-bar.php
@@ -10,4 +10,39 @@
 
 <nav class="vertical menu" id="mobile-menu" role="navigation">
   <?php foundationpress_mobile_nav(); ?>
+
+  <div class="navigationProducts">
+		<a href="#"> View all products </a>
+	<div>
+
+		<div class="productColumn">
+			<h2>Custom Exhibition Stands</h2>
+			<ul>
+				<li><a href="#">Custom</a></li>
+				<li><a href="#">Custom hire</a></li>
+			</ul>
+		</div>
+		<div class="productColumn">
+			<h2>Modular Exhibition Stands</h2>
+			<ul>
+				<li><a href="#">Wave</a></li>
+				<li><a href="#">Fabric</a></li>
+			</ul>
+		</div>
+		<div class="productColumn">
+			<h2>Portable Displays</h2>
+			<ul>
+				<li><a href="#">Ripple</a></li>
+				<li><a href="#">Express pop-up</a></li>
+			</ul>
+		</div>
+		<div class="productColumn">
+			<h2>Counters and accessories</h2>
+			<ul>
+				<li><a href="#">ISObar</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+
 </nav>
