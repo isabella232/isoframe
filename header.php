5e068f191cc6d23939a837b63d972d4649e0f3bc
DaveMaran <davidmaran@davids-mbp.home>
2017-06-15 10:44:56 +0100

    Hover possible

--- header.php
+++ header.php
@@ -46,6 +46,42 @@
 				<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
 					<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
 				<?php endif; ?>
+
+			  <div class="navigationProducts">
+					<a href="#"> View all products </a>
+				<div>
+
+					<div class="productColumn">
+						<h2>Custom Exhibition Stands</h2>
+						<ul>
+							<li><a href="#">Custom</a></li>
+							<li><a href="#">Custom hire</a></li>
+						</ul>
+					</div>
+					<div class="productColumn">
+						<h2>Modular Exhibition Stands</h2>
+						<ul>
+							<li><a href="#">Wave</a></li>
+							<li><a href="#">Fabric</a></li>
+						</ul>
+					</div>
+					<div class="productColumn">
+						<h2>Portable Displays</h2>
+						<ul>
+							<li><a href="#">Ripple</a></li>
+							<li><a href="#">Express pop-up</a></li>
+						</ul>
+					</div>
+					<div class="productColumn">
+						<h2>Counters and accessories</h2>
+						<ul>
+							<li><a href="#">ISObar</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+
 			</div>
 		</nav>
 	</header>
