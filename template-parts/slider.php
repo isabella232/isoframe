90767b57e22b1077a8ab30bdda8467b9efdf3431
DaveMaran <davidmaran@Davids-MacBook-Pro.local>
2017-07-03 08:22:11 +0100

    Alt tags slider

--- template-parts/slider.php
+++ template-parts/slider.php
@@ -6,7 +6,7 @@
 	?>
 
 		<div style="background-image:url('<?php the_sub_field('background_image'); ?>');">
-		<img src="<?php the_sub_field('foreground_image'); ?>">
+		<img src="<?php the_sub_field('foreground_image'); ?>" alt="<?php the_sub_field('title_text', false); ?>">
 			<div class="titleText">
 				<div>
 					<h2><?php the_sub_field('title_text', false); ?></h2>
