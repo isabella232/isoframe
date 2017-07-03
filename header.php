a168c276767375bb824934ec4831d33392d59564
DaveMaran <davidmaran@Davids-MacBook-Pro.local>
2017-07-03 10:32:35 +0100

    Add favicon

--- header.php
+++ header.php
@@ -15,6 +15,14 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<?php wp_head(); ?>
+
+			<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/assets/images/apple-touch-icon.png">
+			<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon-32x32.png">
+			<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon-16x16.png">
+			<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/assets/images/manifest.json">
+			<link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/safari-pinned-tab.svg" color="#5bbad5">
+			<meta name="theme-color" content="#ffffff">
+
 	</head>
 	<body <?php body_class(); ?>>
 	<?php do_action( 'foundationpress_after_body' ); ?>
