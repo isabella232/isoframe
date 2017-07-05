41bcacc9ae66e421ae8919d2440d3b388166c2c3
David Maran <david@katapult.co.uk>
2017-07-05 16:39:06 +0100

    Month and year blog

--- template-parts/news-advice.php
+++ template-parts/news-advice.php
@@ -13,6 +13,8 @@
 		<div>
 			<a href='<?php echo get_permalink($recent["ID"]); ?> '><?php echo get_the_post_thumbnail($recent["ID"]); ?></a>
 			<h2><?php echo $recent["post_title"]; ?></h2>
+			<?php echo date( 'F, Y', strtotime( $recent['post_date'] ) ); ?>
+			<br />
 			<a href='<?php echo get_permalink($recent["ID"]); ?> '>Read more ></a>
 			<?php echo date( 'M', strtotime( $recent['post_date'] ) ); ?>
 		</div>
