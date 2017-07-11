a26601ae73e49ac13bb93155897763d4ecfd236b
David Maran <david@katapult.co.uk>
2017-07-11 11:54:53 +0100

    Search system

--- header.php
+++ header.php
@@ -45,7 +45,7 @@
 				</div>
 				<div class="top-bar-right">
 				<div class="searchbox--frame">
-					<form action="<?php echo site_url(); ?>/products/" method="get" id="searchform">
+					<form action="<?php echo site_url(); ?>" method="get" id="searchform">
 						<input class="searchBox" name="s" placeholder="Search...">
 					</form>
 				</div>
