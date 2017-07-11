028e1fb5efd2071dc026e041fd9f56536cdad9a8
David Maran <david@katapult.co.uk>
2017-07-11 09:18:45 +0100

    Add the date

--- archive.php
+++ archive.php
@@ -18,6 +18,7 @@
  <?php while ( have_posts() ) : the_post(); ?>
   <div class="content">
   	<h1><?php the_title(); ?></h1>
+  	<?php the_date(); ?><br />
            <?php the_content(); ?>
 	</div>
  <?php endwhile;?>
