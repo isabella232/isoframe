6d9a082f220b2ae5081a7f2852966974be35851e
David Maran <david@katapult.co.uk>
2017-07-19 14:17:35 +0100

    Show acf

--- functions.php
+++ functions.php
@@ -222,7 +222,7 @@ function remove_menus(){
   //remove_menu_page( 'edit.php' );                   //Posts
   //remove_menu_page( 'upload.php' );                 //Media
   //remove_menu_page( 'edit.php?post_type=page' );    //Pages
-  remove_menu_page( 'edit.php?post_type=acf-field-group' );    //fields
+  //remove_menu_page( 'edit.php?post_type=acf-field-group' );    //fields
   remove_menu_page( 'admin.php?page=Wordfence' );    //fields
   remove_menu_page( 'edit-comments.php' );          //Comments
   remove_menu_page( 'themes.php' );                 //Appearance
