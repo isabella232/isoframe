49d5e9a1b4d4082c03d8b7663fa9aa986088aa99
David Maran <david@katapult.co.uk>
2017-07-24 11:49:29 +0100

    remarketing tracking

--- footer.php
+++ footer.php
@@ -80,6 +80,21 @@
   ga('send', 'pageview');
 </script>
 
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 1000925128;
+var google_custom_params = window.google_tag_params;
+var google_remarketing_only = true;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/1000925128/?guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
 <?php do_action( 'foundationpress_before_closing_body' ); ?>
 
 <script>
