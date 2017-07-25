c17f4f4f4efe774ddd0cb9d1d9f3ec662daadbd0
David Maran <david@katapult.co.uk>
2017-07-25 14:37:51 +0100

    Button target blank

--- template-parts/designer-pull-out.php
+++ template-parts/designer-pull-out.php
@@ -6,10 +6,10 @@
 		<div class="content--text">
 			<h2><?php the_field('title', 'option'); ?></h2>
 			<?php the_field('content', 'option'); ?>
-			<a href="<?php the_field('link_url', 'option'); ?>" class="button"><?php the_field('link_text', 'option'); ?></a>
+			<a href="<?php the_field('link_url', 'option'); ?>" target="_blank" class="button"><?php the_field('link_text', 'option'); ?></a>
 			<span>Available on 
-			<a href="<?php the_field('android_link', 'option'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/android.svg"></a>
-			<a href="<?php the_field('apple_link', 'option'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apple.svg"></a>
+			<a href="<?php the_field('android_link', 'option'); ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/android.svg"></a>
+			<a href="<?php the_field('apple_link', 'option'); ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apple.svg"></a>
 			<a href="<?php the_field('windows_link', 'option'); ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/windows.svg"></a>
 			</span> 
 		</div>
