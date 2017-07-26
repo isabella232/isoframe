83e6ded748a90abec7e537e2b1730038c5b284d7
David Maran <david@katapult.co.uk>
2017-07-26 10:32:08 +0100

    Add/ change tracking codes

--- header.php
+++ header.php
@@ -26,6 +26,37 @@
 			  <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/2969598.js"></script>
 			<!-- End of HubSpot Embed Code -->
 
+			<script type="text/javascript" src="http://www.cnej4912jks.com/js/110201.js" ></script>
+			<noscript><img src=" " style="display:none;" /></noscript>
+
+			<script>
+			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+			  ga('create', 'UA-41704037-1', 'auto');
+			  ga('send', 'pageview');
+			</script>
+
+			<script type="text/javascript">
+			/* <![CDATA[ */
+			var google_conversion_id = 1000925128;
+			var google_custom_params = window.google_tag_params;
+			var google_remarketing_only = true;
+			/* ]]> */
+			</script>
+			
+			<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+			</script>
+
+			<noscript>
+			<div style="display:inline;">
+			<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/1000925128/?guid=ON&amp;script=0"/>
+			</div>
+			</noscript>
+
+
+
 	</head>
 	<body <?php body_class(); ?>>
 	<?php do_action( 'foundationpress_after_body' ); ?>
