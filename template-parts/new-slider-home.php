d9c54244da4fb95e8dbc4ce213144baadcafb63b
Oliver Stanley <37543310+OStanley94@users.noreply.github.com>
2018-03-21 13:21:21 +0000

    Update new-slider-home.php

--- template-parts/new-slider-home.php
+++ template-parts/new-slider-home.php
@@ -1,1 +1,69 @@
+<div class="homeslider">
 
+	<?php
+	if( have_rows('slider') ):
+	    while ( have_rows('slider') ) : the_row();
+	?>
+		<div class="slider-items" style="width:<?php the_field('slider_1_width', false); ?>;">
+		<img src="<?php the_field('slider_1', false); ?>">
+			<div class="titleText">
+				<div>
+					<h2><?php the_field('title_text', false); ?></h2><?php the_field('sub_text', false); ?>
+				</div>
+			</div>
+		</div>
+
+		<div class="slider-items" style="width:<?php the_field('slider_2_width', false); ?>;">
+		<img src="<?php the_field('slider_2', false); ?>">
+			<div class="titleText">
+				<div>
+					<h2><?php the_field('title_text', false); ?></h2><?php the_field('sub_text', false); ?>
+				</div>
+			</div>
+		</div>
+
+		<div class="slider-items" style="width:<?php the_field('slider_3_width', false); ?>;">
+		<img src="<?php the_field('slider_3', false); ?>">
+			<div class="titleText">
+				<div>
+					<h2><?php the_field('title_text', false); ?></h2><?php the_field('sub_text', false); ?>
+				</div>
+			</div>
+		</div>
+
+		<div class="slider-items" style="width:<?php the_field('slider_1_width', false); ?>;">
+		<img src="<?php the_field('slider_1', false); ?>">
+			<div class="titleText">
+				<div>
+					<h2><?php the_field('title_text', false); ?></h2><?php the_field('sub_text', false); ?>
+				</div>
+			</div>
+		</div>
+
+		<div class="slider-items" style="width:<?php the_field('slider_2_width', false); ?>;">
+		<img src="<?php the_field('slider_2', false); ?>">
+			<div class="titleText">
+				<div>
+					<h2><?php the_field('title_text', false); ?></h2><?php the_field('sub_text', false); ?>
+				</div>
+			</div>
+		</div>
+
+		<div class="slider-items" style="width:<?php the_field('slider_3_width', false); ?>;">
+		<img src="<?php the_field('slider_3', false); ?>">
+			<div class="titleText">
+				<div>
+					<h2><?php the_field('title_text', false); ?></h2><?php the_field('sub_text', false); ?>
+				</div>
+			</div>
+		</div>
+
+	<?php  
+		endwhile;
+	endif;
+	?>
+
+</div>
+
+<div class="sliderprevious"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/previous.svg"></div>
+<div class="slidernext"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/next.svg"></div>
